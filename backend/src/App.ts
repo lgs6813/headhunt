1839e645e47b673485357684453f1161bbbf3b3e
DESKTOP-0QKIBFE\snn77 <snn77@naver.com>
2018-11-15 16:44:17 +0900

    table 생성 sql문 작성

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,7 +1,7 @@
 import * as Koa from "koa";
 import { UserRouter } from "./router/UserRouter";
 import { Context } from "koa";
-import * as NodeCache from "node-cache";
+import * as NodeCache from "node-cache";  
 import { Authentication } from "./library/Authentication";
 import { Connection} from "mysql";
 import { Db} from "./DB/Db";
@@ -11,25 +11,25 @@ export class App{
     private nodeCache: NodeCache;
     private db: Connection;
 
-    constructor(){
+    constructor(){ 
         this.app = new Koa();
-        this.nodeCache = new NodeCache();
-        this.db = new Db().getDB();
-        this.setMiddleware();
+        this.nodeCache = new NodeCache(); 
+        this.db = new Db().getDB(); 
+        this.setMiddleware();  
         this.setRoutes();
-    }
+    } 
 
-    public setMiddleware(){
+    public setMiddleware(){ 
         const auth = new Authentication(this.nodeCache);
         this.app.use((ctx: Context, next: Function) => auth.authentication(ctx, next));
     } 
- 
-    public setRoutes(){
+
+    public setRoutes(){ 
         const userRouter = new UserRouter(this.db).getRouter();
         this.app.use(userRouter.routes()).use(userRouter.allowedMethods())
     }
 
-    public getApp(){ 
-        return this.app;
+    public getApp(){    
+        return this.app; 
     }
 }
--- backend/src/DB/Db.ts
+++ backend/src/DB/Db.ts
@@ -1,15 +1,19 @@
 import * as mysql from "mysql";
-import {Config} from "../config/Config";
+import { Config } from "../config/Config";
 
-export class Db{
+export class Db {
     private db: mysql.Connection;
 
-    constructor(){
+    constructor() {
         this.db = mysql.createConnection(Config.databaseOption);
-        this.db.connect();
-    } 
+        this.db.connect((err) => {
+            if (err) {
+                console.log("database connect error", err);
+            }
+        });
+    }
 
-    public getDB(){
+    public getDB() {
         return this.db;
     }
 }
